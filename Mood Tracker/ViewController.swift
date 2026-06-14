fed92695ff04e5a5361db10a974094d6a2fdc3ce
RTV <rtvconcepts@gmail.com>
2026-06-14 16:58:15 -0400

    emojis showing in label

--- Mood Tracker/ViewController.swift
+++ Mood Tracker/ViewController.swift
@@ -17,15 +17,46 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var savedDate: UILabel!
     
+    var currentEmoji = "😐"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func loadInitUI() {
+        moodLabel.text = "Netural 😐"
+        savedDate.text = "No saved moods"
+    }
+    
     @IBAction func sliderChanged(_ sender: UISlider) {
+        let sliderVal = Int(sender.value)
+        
+        switch sliderVal {
+        case 0 ... 20:
+            moodLabel.text = "Very Sad 😭"
+            currentEmoji = "😭"
+        case 21...40:
+            moodLabel.text = "Sad 😥"
+            currentEmoji = "😥"
+        case 41...60:
+            moodLabel.text = "Neutral 😐"
+            currentEmoji = "😐"
+        case 61...80:
+            moodLabel.text = "Happy 😊"
+            currentEmoji = "😊"
+        case 81...100:
+            moodLabel.text = "Very happy 😃"
+            currentEmoji = "😃"
+        default:
+            break
+        }
     }
     
     @IBAction func saveMood(_ sender: UIButton) {
     }
+    
+    
+  // 😃  😊   😐   😥   😭
 }
 
